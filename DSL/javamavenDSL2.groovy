b88d3327bd299774501dcbf3ec085c1820af2bdb
puchevcp <123635253+puchevcp@users.noreply.github.com>
2024-05-27 10:25:43 -0300

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,7 +1,7 @@
 job('Java-Maven-App-DSL-2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/puchevcp/simple-java-maven-app.git', 'master') { node ->
+        git('https://github.com/puchevcp/simple-java-maven-app.git', 'main') { node ->
             node / gitConfigName('puchevcp')
             node / gitConfigEmail('puche.vcp@gmail.com')
         }
